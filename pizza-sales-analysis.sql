a6ea28b038269d9836ef84adb44f48d4d00d2d6a
Krunali Warake <warkechetna@gmail.com>
2026-04-01 15:23:32 +0530

    Add files via upload

---
+++ pizza-sales-analysis.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE pizza;
+USE pizza;
+
+SELECT* FROM pizza_sales;
+-- 1. Total Revenue:
+SELECT SUM(total_price) AS Total_Revenue FROM pizza_sales;
+
+-- 2. Average Order Value
+SELECT (SUM(total_price) / COUNT(DISTINCT order_id)) AS Avg_order_Value FROM pizza_sales;
+
+-- 3. Total Pizzas Sold
+SELECT SUM(quantity) AS Total_pizza_sold FROM pizza_sales;
+
+-- 4. Total Orders
+SELECT COUNT(DISTINCT order_id) AS Total_Orders FROM pizza_sales;
+
+-- 5. Average Pizzas Per Order
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
+AS Avg_Pizzas_per_order
+FROM pizza_sales;
+
+-- G. Top 5 Pizzas by Revenue
+SELECT pizza_name, SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue DESC LIMIT 5;
+
+-- H. Bottom 5 Pizzas by Revenue
+SELECT pizza_name, SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue ASC LIMIT 5;
+
+-- I. Top 5 Pizzas by Quantity
+SELECT  pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold DESC LIMIT 5;
+
+-- J. Bottom 5 Pizzas by Quantity
+SELECT pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold ASC LIMIT 5;
+
+-- K. Top 5 Pizzas by Total OrderS
+SELECT pizza_name, COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Orders DESC LIMIT 5;
+
+-- L. Bottom 5 Pizzas by Total Orders
+SELECT pizza_name, COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Orders ASC LIMIT 5;
+
+
+
+
+
+
+
+
+
